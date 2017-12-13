9670c90432a7f28760466527535f938428a7f285
zeyap <zeyap@qq.com>
2017-12-13 22:13:20 +0800

    threshold estimation

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 #include "opencv2/highgui.hpp"
 using namespace std;
 using namespace cv;
-#define DELAY_CAPTION 1500
+#define DELAY_CAPTION 150
 #define DELAY_DISPLAY 100
 #define KERNEL_LENGTH 7
-#define LOWER_THRES 6
-#define HIGHER_THRES 11
+#define HIGH_THRESHOLD_PERCENTAGE 0.2f
 #define PI 3.14f
 Mat src; 
 Mat dst; 
@@ -25,10 +24,11 @@ struct Gradient {
 	float* N;
 };
 
-Gradient gradient(Mat src, Mat &dst);
+Gradient gradient_sobel(Mat src, Mat &dst);
 void nonMaxSuppression(Mat src, Mat &dst, Gradient &g);
 void Sector(Gradient &g);
 void thresholding(Mat src, Mat &dst, uchar t);
+void estimateThreshold(Mat src, uchar* high, uchar* low);
 void doubleThresholding(Mat src, Mat &dst, uchar tl, uchar th);
 void hysteresis(Mat &dst, Mat ldst);
 bool findBreakpoint(int i, int j);
@@ -63,7 +63,7 @@ int main(int argc, char ** argv)
 	if (display_dst(DELAY_CAPTION) != 0) { return 0; }
 	
 	if (display_caption("Gradient") != 0) { return 0; }
-	Gradient g = gradient(src, dst);
+	Gradient g = gradient_sobel(src, dst);
 	src = dst.clone();
 	if (display_dst(DELAY_CAPTION) != 0) { return 0; }
 	
@@ -74,12 +74,16 @@ int main(int argc, char ** argv)
 	
 	Mat newDst = src.clone();
 
+	uchar thigh = 0;
+	uchar tlow = 0;
+	estimateThreshold(src, &thigh, &tlow);
+
 	if (display_caption("Higher Thresholding") != 0) { return 0; }
-	thresholding(src, newDst, HIGHER_THRES);
+	thresholding(src, newDst, thigh);
 	if (display_dst(DELAY_CAPTION, newDst) != 0) { return 0; }
 
 	if (display_caption("Double Thresholding") != 0) { return 0; }
-	doubleThresholding(src, dst, LOWER_THRES, HIGHER_THRES);
+	doubleThresholding(src, dst, thigh, tlow);
 	src = dst.clone();
 	if (display_dst(DELAY_CAPTION) != 0) { return 0; }
 
@@ -110,7 +114,7 @@ int display_dst(int delay)
 	return display_dst(delay,dst);
 }
 
-Gradient gradient(Mat src, Mat &dst) {
+Gradient gradient_sobel(Mat src, Mat &dst) {
 
 	float gradient[2];
 
@@ -120,8 +124,20 @@ Gradient gradient(Mat src, Mat &dst) {
 	
 	for (int i = 1; i < h-1;i++) {
 		for (int j = 1; j < w-1; j++) {
-			gradient[0]=(src.at<uchar>(i,j+1)-src.at<uchar>(i,j)+ src.at<uchar>(i+1, j+1)- src.at<uchar>(i+1, j))*0.5f;
-			gradient[1] = (src.at<uchar>(i, j) - src.at<uchar>(i+1, j) + src.at<uchar>(i, j + 1) - src.at<uchar>(i + 1, j+1))*0.5f;
+			gradient[0]=
+				-src.at<uchar>(i-1,j-1)
+				+src.at<uchar>(i-1,j+1)
+				-2* src.at<uchar>(i, j-1)
+				+2* src.at<uchar>(i, j + 1)
+				- src.at<uchar>(i+1, j-1) 
+				+ src.at<uchar>(i + 1, j+1);
+			gradient[1] = 
+				src.at<uchar>(i - 1, j - 1)
+				+2* src.at<uchar>(i - 1, j)
+				+ src.at<uchar>(i - 1, j + 1)
+				- src.at<uchar>(i+1, j - 1)
+				-2* src.at<uchar>(i + 1, j)
+				- src.at<uchar>(i + 1, j + 1);
 			g.M[i*w + j] = sqrt(gradient[0]*gradient[0]+gradient[1]* gradient[1]);
 			g.theta[i*w + j] = atan2(gradient[1],gradient[0]);
 			dst.at<uchar>(i, j) = (uchar)g.M[i*w + j];
@@ -201,12 +217,42 @@ void thresholding(Mat src, Mat &dst, uchar t) {
 	}
 }
 
-void doubleThresholding(Mat src, Mat &dst, uchar tl, uchar th) {
+void doubleThresholding(Mat src, Mat &dst, uchar th, uchar tl) {
 	Mat ldst;
 	ldst=src.clone();
 	thresholding(src, dst, th);
 	thresholding(src, ldst, tl);
 	hysteresis(dst,ldst);
+}
+
+void estimateThreshold(Mat src, uchar* high, uchar* low) {
+	int histogram[256];
+	uchar max = 0;
+	int totalPixelNum = 0;
+	for (int i = 0; i < 256; i++) {
+		histogram[i] = 0;
+	}
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			int temp = src.at<uchar>(i, j);
+			if (temp > max) {
+				max = temp;
+			}
+			histogram[temp]++;
+		}
+	}
+	for (int i = 0; i < 255; i++) {
+		totalPixelNum+=histogram[i];
+	}
+	int pixelNum = (totalPixelNum - histogram[0])*HIGH_THRESHOLD_PERCENTAGE;
+	int highCutoff = max;
+	int beyondCutoffSum=0;
+	do {
+		beyondCutoffSum += histogram[highCutoff];
+		highCutoff--;
+	} while (beyondCutoffSum < pixelNum);
+	*high = highCutoff;
+	*low = highCutoff*0.5;
 }
 
 void hysteresis(Mat &dst, Mat ldst) {
